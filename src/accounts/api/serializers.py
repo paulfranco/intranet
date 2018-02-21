c1c3c812bee7cc65dd231dcddfe80d897dabf8c1
Paul Franco <paulfranco@me.com>
2018-02-21 03:22:35 -0800

    added users and followers

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import get_user_model
 
+from django.urls import reverse_lazy
+
 from rest_framework import serializers
 
 User = get_user_model()
 
 class UserDisplaySerializer(serializers.ModelSerializer):
 	follower_count = serializers.SerializerMethodField()
+	url = serializers.SerializerMethodField()
 	class Meta:
 		model = User
 		fields = [
@@ -13,9 +16,13 @@ class UserDisplaySerializer(serializers.ModelSerializer):
 			'first_name',
 			'last_name',
 			'follower_count',
+			'url',
 			#'email',
 		]
 
 	def get_follower_count(self, obj):
 		#print(obj.username)
-		return 0
+		return 
+
+	def get_url(self, obj):
+		return reverse_lazy("profiles:detail", kwargs={"username": obj.username })
